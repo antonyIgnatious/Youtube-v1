5f91fa3d9e765323f38fb0c5c3f2ff78f37e685f
Antony Ignatious <antonyignatious@ymail.com>
2017-08-16 01:19:45 +0530

    added ajax call for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,6 @@
 class SearchController < ApplicationController
-
   require 'google/api_client'
-  
 
-  # Set DEVELOPER_KEY to the API key value from the APIs & auth > Credentials
-  # tab of
-  # {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
-  # Please ensure that you have enabled the YouTube Data API for your project.
   DEVELOPER_KEY = ENV['YOUTUBE_API']
   YOUTUBE_API_SERVICE_NAME = 'youtube'
   YOUTUBE_API_VERSION = 'v3'
@@ -19,12 +13,10 @@ class SearchController < ApplicationController
       :application_version => '1.0.0'
     )
     youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
-
     return client, youtube
   end
 
   def videos_Search
-
     client, youtube = get_service
     begin
       search_response = client.execute!(
@@ -32,35 +24,16 @@ class SearchController < ApplicationController
         :parameters => {
           :part => 'snippet',
           :q => params[:query],
-          :maxResults => '50'
+          :maxResults => '50',
+          :type => 'video'
         }
       )
-
-      videos = []
-      channels = []
-      playlists = []
-      response_data = []
-      # p search_response.to_json
-      # Add each result to the appropriate list, and then display the lists of
-      # matching videos, channels, and playlists.
-      search_response.data.items.each do |search_result|
-        case search_result.id.kind
-          when 'youtube#video'
-            videos << "#{search_result.snippet.title} (#{search_result.id.videoId})"
-            response_data << { :title => search_result.snippet.title, 
-                              :videoid => search_result.id.videoId}
-          when 'youtube#channel'
-            channels << "#{search_result.snippet.title} (#{search_result.id.channelId})"
-          when 'youtube#playlist'
-            playlists << "#{search_result.snippet.title} (#{search_result.id.playlistId})"
-        end
-      end
-      puts response_data.to_json
-      puts "Videos:\n", videos, "\n"
-      puts "Channels:\n", channels, "\n"
-      puts "Playlists:\n", playlists, "\n"
+      render :json => {:status => 'success', :data => search_response.body}
     rescue Google::APIClient::TransmissionError => e
       puts e.result.body
     end
+  end
+
+  def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'search/index'
-  post "videos_Search" => "search#videos_Search", as: :videos_Search
+  get "index" => "search#index"
+  get "videos_Search" => "search#videos_Search", as: :videos_Search
   root to:'search#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
